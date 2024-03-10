7a6500525a67c0e148fd613f65d6041807ce1b6a
Humberto Inzunza <josehinzunza@hotmail.com>
2024-03-10 13:43:01 -0700

    gp3-jsm first steps

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import "./navbar.css";
+
+function Navbar() {
+    return(
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/containers/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+    return(
+        <div>
+            Blog
+        </div>
+    )
+}
+
+export default Header;
